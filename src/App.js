e0adffd6c403d461b8ff413cf8954340e3118563
xiaobai <1451730457@qq.com>
2020-12-24 20:12:39 +0800

    update

--- src/App.js
+++ src/App.js
@@ -1,18 +1,11 @@
-import { Component } from "react";
-import { Route, Switch } from "react-router-dom";
-import 'antd/dist/antd.less';
-import Login from "./pages/login/login.jsx";
-import Admin from "./pages/admin/admin.jsx";
+import React, {Component} from "react";
 
 
 export default class App extends Component{
   render(){
-    return (
-      <div className="app">
-        <Switch>
-          <Route path='/login' component={Login}/>
-          <Route path='/admin' component={Admin}/>
-        </Switch>
+    return(
+      <div>
+        App
       </div>
     )
   }
